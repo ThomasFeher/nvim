2ead83fbb94822b02ed640bb54fe034fac11965f
Thomas Feher <thomasfeher@web.de>
2022-12-14 21:44:40 +0100

    Move from NerdCommenter to mini.comment
    NerdCommenter crashes sometimes.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -192,8 +192,18 @@ return require('packer').startup(function(use)
 		--   <BS>: delete some pre-steps
 		--
 		require('mini.align').setup()
+		require('mini.comment').setup{
+			mappings = {
+				-- Toggle comment (like `gcip` - comment inner paragraph) for both
+				-- Normal and Visual modes
+				comment = 'gc',
+				-- Toggle comment on current line
+				comment_line = 'gcc',
+				-- Define 'comment' textobject (like `dgc` - delete whole comment block)
+				textobject = 'gc',
+			},
+		}
 	end }
-	use 'scrooloose/nerdcommenter'
 	use 'Raimondi/delimitMate'
 	use 'PProvost/vim-ps1'
 	use 'bronson/vim-trailing-whitespace'
