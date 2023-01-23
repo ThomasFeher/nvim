3f69ee6b7766a6f5f7a890a41109d70fe68c6d79
Thomas Fehér <thomas.feher@voiceinterconnect.de>
2023-01-23 09:39:15 +0100

    Vimtex: Adjust to using treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -24,6 +24,8 @@ return require('packer').startup({function(use)
 			vim.g.vimtex_view_general_viewer = 'okular'
 			vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
 			vim.g.vimtex_compiler_latexmk = { build_dir = 'build' }
+			vim.g.vimtex_syntax_enabled = 0  -- using treesitter for syntax highlighting
+			vim.g.vimtex_syntax_conceal_disable = 1  -- no conceal available when using treesitter
 		end,
 		}
 
