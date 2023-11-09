6dd9f3422a96a7f169e5cb69bd7af3cac0e442a0
Thomas Fehér <thomas.feher@voiceinterconnect.de>
2023-11-09 13:31:19 +0100

    Fix LuaSnip configuration

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -30,7 +30,7 @@ return require('lazy').setup({
 	'honza/vim-snippets',
 	{ "L3MON4D3/LuaSnip",
 		-- follow latest release.
-		version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 		-- install jsregexp (optional!).
 		build = "make install_jsregexp",
 		config = function ()
