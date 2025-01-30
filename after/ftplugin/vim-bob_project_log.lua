8ffc51de230b5cf0653f799cb7859372d71cfb48
Thomas Fehér <thomas.feher@voiceinterconnect.de>
2025-01-30 16:51:30 +0100

    Do not lint generated file

--- after/ftplugin/vim-bob_project_log.lua
+++ after/ftplugin/vim-bob_project_log.lua
@@ -6,3 +6,5 @@ vim.keymap.set('n', '<leader><CR>', '<cmd>BobProjectNoBuild<CR>', {
 	desc = 'Start project that is logged under the cursor without re-building it.',
 	buffer = true,
 })
+-- no need for linting a generated file
+vim.cmd('ALEDisableBuffer')
